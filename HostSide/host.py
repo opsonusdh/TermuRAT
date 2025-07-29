b827c512fd2d352d5b9b171595c29c8457b9b373
opsonusdh <sumanasensen262@gmail.com>
2025-07-29 18:18:55 +0530

    Update host.py

--- HostSide/host.py
+++ HostSide/host.py
@@ -75,7 +75,7 @@ while True:
     received = read_gist(data["to_host_gist_id"])
     
     received = json.loads(received)
-    if received.get("time") == last_received_time or not recived:
+    if received.get("time") == last_received_time or not received:
         time.sleep(3)
         continue
     
